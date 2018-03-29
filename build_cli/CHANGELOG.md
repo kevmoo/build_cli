81748c032d77adabed1dc3cb345bc695fcaee5f1
Kevin Moore <kevmoo@google.com>
2018-03-29 16:36:43 -0700

    Consolidate library files into `build_cli.dart`

--- build_cli/CHANGELOG.md
+++ build_cli/CHANGELOG.md
@@ -1,8 +1,11 @@
-## 0.1.1
+## 0.2.0
 
 - Use the logger built into `pkg:build`.
 - Don't generate CLI entries for unassignable fields.
 - Support latest `pkg:source_gen`. 
+- *BREAKING* (strictly speaking, but not likely.)
+  - Removed `package:build_cli/builder.dart`.
+  - Moved and renamed the builder factory to `build_cli.dart`.
 
 ## 0.1.0
 
--- build_cli/lib/build_cli.dart
+++ build_cli/lib/build_cli.dart
@@ -1,1 +1,45 @@
+/// Configuration for using `package:build`-compatible build systems.
+///
+/// This library is **not** intended to be imported by typical end-users unless
+/// you are creating a custom compilation pipeline.
+///
+/// See [package:build_runner](https://pub.dartlang.org/packages/build_runner)
+/// for more information.
+library build_cli;
+
+import 'package:build/build.dart';
+import 'package:logging/logging.dart';
+import 'package:source_gen/source_gen.dart';
+
+import 'src/build_cli_generator.dart';
+
 export 'src/build_cli_generator.dart';
+
+// TODO: until we can use `log` here - github.com/dart-lang/build/issues/1223
+final _logger = new Logger('json_serializable');
+
+/// Supports `package:build_runner` creation and configuration of `build_cli`.
+///
+/// Not meant to be invoked by hand-authored code.
+Builder createBuilderForCliGenerator(BuilderOptions options) {
+  // Paranoid copy of options.config - don't assume it's mutable or needed
+  // elsewhere.
+  var optionsMap = new Map<String, dynamic>.from(options.config);
+
+  try {
+    return _cliPartBuilder(header: optionsMap.remove('header') as String);
+  } finally {
+    if (optionsMap.isNotEmpty) {
+      _logger.warning('These options were ignored: `$optionsMap`.');
+    }
+  }
+}
+
+Builder _cliPartBuilder({String header}) {
+  return new PartBuilder(
+    const [
+      const CliGenerator(),
+    ],
+    header: header,
+  );
+}
