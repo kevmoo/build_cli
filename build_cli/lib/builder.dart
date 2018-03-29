37318869d5ed59c26a03072aa72a65a94df4d44e
Kevin Moore <kevmoo@google.com>
2018-03-29 12:02:22 -0700

    Warn if unused options are provided
    Also cleanup builder.dart

--- build_cli/lib/builder.dart
+++ build_cli/lib/builder.dart
@@ -1,24 +1,33 @@
-// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'package:build/build.dart';
 import 'package:logging/logging.dart';
+import 'package:source_gen/source_gen.dart';
 
-import 'src/part_builder.dart';
+import 'src/build_cli_generator.dart';
 
+// TODO: until we can use `log` here - github.com/dart-lang/build/issues/1223
 final _logger = new Logger('json_serializable');
 
 Builder cliBuilder(BuilderOptions options) {
-  var setOptions = new Map<String, dynamic>.from(options.config);
+  // Paranoid copy of options.config - don't assume it's mutable or needed
+  // elsewhere.
+  var optionsMap = new Map<String, dynamic>.from(options.config);
 
-  dynamic read(String key) => setOptions.remove(key);
+  _logger.warning(StackTrace.current);
 
   try {
-    return cliPartBuilder(header: read('header') as String);
+    return _cliPartBuilder(header: optionsMap.remove('header') as String);
   } finally {
-    if (setOptions.isNotEmpty) {
-      _logger.warning('These options were ignored: `$setOptions`.');
+    if (optionsMap.isNotEmpty) {
+      _logger.warning('These options were ignored: `$optionsMap`.');
     }
   }
+}
+
+Builder _cliPartBuilder({String header}) {
+  return new PartBuilder(
+    const [
+      const CliGenerator(),
+    ],
+    header: header,
+  );
 }
