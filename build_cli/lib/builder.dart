d8f4d03e126480d9c053f9ce0dc593175255cf22
Kevin Moore <kevmoo@google.com>
2018-04-07 19:36:34 -0700

    Simplify things using new build_* features

--- build_cli/lib/builder.dart
+++ build_cli/lib/builder.dart
@@ -8,26 +8,25 @@
 library builder;
 
 import 'package:build/build.dart';
-import 'package:logging/logging.dart';
 import 'package:source_gen/source_gen.dart';
 
 import 'src/build_cli_generator.dart';
-
-// TODO: until we can use `log` here - github.com/dart-lang/build/issues/1223
-final _logger = new Logger('json_serializable');
 
 Builder buildCli(BuilderOptions options) {
   // Paranoid copy of options.config - don't assume it's mutable or needed
   // elsewhere.
   var optionsMap = new Map<String, dynamic>.from(options.config);
 
-  try {
-    return _cliPartBuilder(header: optionsMap.remove('header') as String);
-  } finally {
-    if (optionsMap.isNotEmpty) {
-      _logger.warning('These options were ignored: `$optionsMap`.');
+  var builder = _cliPartBuilder(header: optionsMap.remove('header') as String);
+
+  if (optionsMap.isNotEmpty) {
+    if (log == null) {
+      throw new StateError('Requires build_runner >=0.8.2 – please upgrade.');
     }
+    log.warning('These options were ignored: `$optionsMap`.');
   }
+
+  return builder;
 }
 
 Builder _cliPartBuilder({String header}) {
