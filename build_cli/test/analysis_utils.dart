753266116688e1d5ba25fb74c57eb9b73b87ca98
Kevin Moore <kevmoo@users.noreply.github.com>
2018-09-05 15:20:05 -0700

    misc: update tests to use public pkg:analyzer APIs (#20)
    Update dependency on pkg:analyzer to latest

--- build_cli/test/analysis_utils.dart
+++ build_cli/test/analysis_utils.dart
@@ -3,65 +3,17 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:io';
 
-import 'package:analyzer/file_system/file_system.dart' hide File;
-import 'package:analyzer/file_system/physical_file_system.dart';
-import 'package:analyzer/src/source/package_map_resolver.dart';
-import 'package:analyzer/src/source/pub_package_map_provider.dart';
-import 'package:analyzer/src/dart/sdk/sdk.dart' show FolderBasedDartSdk;
+import 'package:build/build.dart';
 import 'package:analyzer/src/generated/engine.dart';
-import 'package:analyzer/src/generated/source.dart';
-import 'package:analyzer/src/generated/source_io.dart';
-import 'package:path/path.dart' as p;
+import 'package:build_test/build_test.dart';
 
-Future<AnalysisContext> getAnalysisContextForProjectPath(
-    String projectPath) async {
-  var sdkPath = _getSdkPath();
+Future<AnalysisContext> getAnalysisContextForProjectPath() async {
+  var library = await resolveAsset(
+      AssetId.parse('build_cli|test/src/test_input.dart'), (resolver) async {
+    var allLibs = await resolver.libraries.toList();
+    return allLibs.first;
+  });
 
-  var resourceProvider = PhysicalResourceProvider.INSTANCE;
-  var sdk = new FolderBasedDartSdk(
-      resourceProvider, resourceProvider.getFolder(sdkPath));
-
-  var packageResolver = _getPackageResolver(projectPath, sdk);
-
-  var resolvers = [
-    new DartUriResolver(sdk),
-    new ResourceUriResolver(PhysicalResourceProvider.INSTANCE),
-    packageResolver
-  ];
-
-  AnalysisEngine.instance.processRequiredPlugins();
-
-  var options = new AnalysisOptionsImpl()..analyzeFunctionBodies = false;
-
-  var context = AnalysisEngine.instance.createAnalysisContext()
-    ..analysisOptions = options
-    ..sourceFactory = new SourceFactory(resolvers);
-
-  return context;
+  return library.context;
 }
-
-UriResolver _getPackageResolver(String projectPath, FolderBasedDartSdk sdk) {
-  var dotPackagesPath = p.join(projectPath, '.packages');
-
-  if (!FileSystemEntity.isFileSync(dotPackagesPath)) {
-    throw new StateError('A package configuration file was not found at the '
-        'expectetd location. $dotPackagesPath');
-  }
-
-  var pubPackageMapProvider =
-      new PubPackageMapProvider(PhysicalResourceProvider.INSTANCE, sdk);
-  var packageMapInfo = pubPackageMapProvider.computePackageMap(
-      PhysicalResourceProvider.INSTANCE.getResource(projectPath) as Folder);
-  var packageMap = packageMapInfo.packageMap;
-  if (packageMap == null) {
-    throw new StateError('An error occurred getting the package map.');
-  }
-
-  return new PackageMapUriResolver(
-      PhysicalResourceProvider.INSTANCE, packageMap);
-}
-
-/// Return the path to the current Dart SDK.
-String _getSdkPath() => p.dirname(p.dirname(Platform.resolvedExecutable));
