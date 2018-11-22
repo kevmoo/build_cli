33d3734ff81ce37e00ff198bf86f48510f683774
Alexey Zolotykh <alexey.zolotykh@team.wrike.com>
2018-11-22 14:11:19 +0300

    Add integration test for nullable option

---
+++ build_cli/test/src/undefined_boolean_example.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'undefined_boolean_example.dart';
+
+// **************************************************************************
+// CliGenerator
+// **************************************************************************
+
+NullableOptions _$parseNullableOptionsResult(ArgResults result) =>
+    new NullableOptions(result['nullable-option'] as bool);
+
+ArgParser _$populateNullableOptionsParser(ArgParser parser) =>
+    parser..addFlag('nullable-option', defaultsTo: null);
+
+final _$parserForNullableOptions =
+    _$populateNullableOptionsParser(new ArgParser());
+
+NullableOptions parseNullableOptions(List<String> args) {
+  var result = _$parserForNullableOptions.parse(args);
+  return _$parseNullableOptionsResult(result);
+}
