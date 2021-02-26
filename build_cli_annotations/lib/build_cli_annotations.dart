728f95078e5eebc5d07e57c3ab03d7ae35445180
John Ryan <ryjohn@google.com>
2021-02-26 14:10:27 -0800

    Make CliOption fields nullable

--- build_cli_annotations/lib/build_cli_annotations.dart
+++ build_cli_annotations/lib/build_cli_annotations.dart
@@ -5,33 +5,34 @@ class CliOptions {
 }
 
 class CliOption {
-  final String name;
-  final String abbr;
-  final Object defaultsTo;
-  final String help;
-  final String valueHelp;
-  final List<Object> allowed;
-  final Map<Object, String> allowedHelp;
-  final bool negatable;
-  final bool hide;
-  final bool nullable;
-  final bool provideDefaultToOverride;
+  final String? name;
+  final String? abbr;
+  final Object? defaultsTo;
+  final String? help;
+  final String? valueHelp;
+  final List<Object>? allowed;
+  final Map<Object, String>? allowedHelp;
+  final bool? negatable;
+  final bool? hide;
+  final bool? nullable;
+  final bool? provideDefaultToOverride;
 
   /// A top-level [Function] that converts an option value into the destination
   /// type.
   final dynamic Function(String)? convert;
 
-  const CliOption(
-      {required this.name,
-       required this.abbr,
-        required this.defaultsTo,
-        required this.help,
-        required this.valueHelp,
-        required this.allowed,
-        required this.negatable,
-        required this.allowedHelp,
-        required this.hide,
-        this.convert,
-        required this.nullable,
-        this.provideDefaultToOverride = false});
+  const CliOption({
+    this.name,
+    this.abbr,
+    this.defaultsTo,
+    this.help,
+    this.valueHelp,
+    this.allowed,
+    this.negatable,
+    this.allowedHelp,
+    this.hide,
+    this.convert,
+    this.nullable,
+    this.provideDefaultToOverride = false,
+  });
 }
