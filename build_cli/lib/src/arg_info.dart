81dfc34401a319052f0bce6642fcf81e20639112
John Ryan <ryjohn@google.com>
2021-03-05 14:14:25 -0800

    remove unused import

--- build_cli/lib/src/arg_info.dart
+++ build_cli/lib/src/arg_info.dart
@@ -1,6 +1,5 @@
 import 'package:analyzer/dart/constant/value.dart';
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/dart/element/nullability_suffix.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:build_cli_annotations/build_cli_annotations.dart';
 import 'package:source_gen/source_gen.dart';
