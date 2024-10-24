21429eb339fb56d4dcf9a144a792e04f6c4c1aad
Kevin Moore <kevmoo@users.noreply.github.com>
2024-10-23 17:31:28 -0700

    Fix tests affected by pkg:args changes (#158)

--- build_cli/test/pubviz_integration_test.dart
+++ build_cli/test/pubviz_integration_test.dart
@@ -16,8 +16,11 @@ void main() {
 
   group('with invalid args', () {
     final items = {
-      'Could not find an option named "no-help".': ['--no-help'],
-      '"foo" is not an allowed value for option "format".': ['--format', 'foo'],
+      'Could not find an option named "--no-help".': ['--no-help'],
+      '"foo" is not an allowed value for option "--format".': [
+        '--format',
+        'foo',
+      ],
       'Cannot parse "3.14" into `int` for option "production-port".': [
         '--production-port',
         '3.14',
