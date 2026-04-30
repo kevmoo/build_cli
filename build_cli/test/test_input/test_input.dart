b4b5bfa32da771013e3ae1715b02bc0b67a65105
kevmoo <kevmoo@google.com>
2026-04-30 16:45:47 -0700

    Cleanup ignores while we're here!

--- build_cli/test/test_input/test_input.dart
+++ build_cli/test/test_input/test_input.dart
@@ -373,7 +373,7 @@ PrivateCtor parsePrivateCtor(List<String> args) {
 class PrivateCtor {
   final bool? flag;
 
-  // ignore: unused_element, unused_element_parameter
+  // ignore: unused_element_parameter
   PrivateCtor._({this.flag});
 }
 
